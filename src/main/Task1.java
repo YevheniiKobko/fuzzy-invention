ea069a612d21d48419a9ddb07a14a90486220684
YevheniiKobko <zhenyakobkoiua@gmail.com>
2020-02-13 19:31:42 +0200

    Программа выводит суму двох чисел

--- src/main/Task1.java
+++ src/main/Task1.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        System.out.println("Введи первое число:");
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println("Введите второе число:");
+        int b = scan.nextInt();
+        int sum = a+b;
+        System.out.println("Сума ваших чисел: " + sum + ".");
     }
 }
